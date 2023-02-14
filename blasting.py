e37420ba5183ca52c3d6d2a94a2ba3b051c8edf8
actapia <andrew.tapia@uky.edu>
2023-02-14 05:14:40 -0500

    Document classes and functions.

--- blasting.py
+++ blasting.py
@@ -1,5 +1,6 @@
 import subprocess
 import pandas as pd
+from typing import List
 
 default_out_columns = ['qseqid',
  'sseqid',
@@ -15,14 +16,64 @@ default_out_columns = ['qseqid',
  'bitscore']
 
 class BlastnSearch:
+    """A search (alignment) to be made with blastn.
+
+    This class provides a programmer-friendly way to define the parameters of a
+    simple blastn search, carry out the search, and parse the results.
+
+    The most useful property of a BlastnSearch instance is hits. hits runs the
+    defined blastn search (if it hasn't been run already), parses the results,
+    stores them in a pandas dataframe, and returns the result.
+
+    This class has no public attributes; the settings defined in the
+    constructor should be consided immutable. Nevertheless, the values passed to
+    the constructor may be retrieved through the class's properties.
+    """
     def __init__(
             self,
-            seq1_path,
-            seq2_path,
-            evalue=1e-20,
-            out_columns=default_out_columns,
-            additional_columns=[]
+            seq1_path: str,
+            seq2_path: str,
+            evalue: float = 1e-20,
+            out_columns: List[str] = default_out_columns,
+            additional_columns: List[str] = []
     ):
+        """Construct a BlastnSearch with the specified settings.
+
+        This constructor requires paths to FASTA files containing the query and
+        subject sequences to use in the search.
+
+        Optionally, the caller may provide an expect value cutoff to use for the
+        search. If no value is provided, a default evalue of 1e-20 will be used.
+
+        The caller may specify what columns should be included in the output.
+        By default, the included columns are
+
+            sseqid
+            pident
+            length
+            mismatcch
+            gapopen
+            qstart
+            qend
+            sstart
+            send
+            evalue
+            bitscore
+
+        Explanations of these columns may be found at
+        https://www.metagenomics.wiki/tools/blast/blastn-output-format-6
+
+        If the caller desires to include additional columns, it may provide
+        them to the additional_columns parameter.
+
+        Parameters:
+            seq1_path (str):    Path to query sequence FASTA file.
+            seq2_path (str):    Path to subject sequence FASTA file.
+            evalue (float):     Expect value cutoff to use in BLAST search.
+            out_columns:        Output columns to include in results.
+            additional_columns: Additional output columns to include in results.
+
+        """
         self._seq1_path = seq1_path
         self._seq2_path = seq2_path
         self._evalue = evalue
@@ -30,28 +81,32 @@ class BlastnSearch:
         self._out_columns = list(out_columns + additional_columns)
 
     @property
-    def seq1_path(self):
+    def seq1_path(self) -> str:
+        """Return the query sequence path."""
         return self._seq1_path
 
     @property
-    def seq2_path(self):
+    def seq2_path(self) -> str:
+        """Return the subject sequence path."""
         return self._seq2_path
 
     @property
-    def evalue(self):
+    def evalue(self) -> float:
+        """Return the expect value used as a cutoff in the blastn search."""
         return self._evalue
 
     @property
-    def hits(self):
+    def hits(self) -> pd.DataFrame:
+        """Return a dataframe containing this search's BLAST results."""
         if self._hits is None:
             self._get_hits()
         return self._hits
 
-    def __len__(self):
-        return len(self.hits)
+    # def __len__(self) -> int:
+    #     return len(self.hits)
 
-    def __iter__(self):
-        yield from self.hits
+    # def __iter__(self):
+    #     yield from self.hits
 
     def _get_hits(self):
         proc = subprocess.Popen(
@@ -70,4 +125,8 @@ class BlastnSearch:
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        self._hits = pd.read_csv(proc.stdout, names=self._out_columns, delim_whitespace=True)
+        self._hits = pd.read_csv(
+            proc.stdout,
+            names=self._out_columns,
+            delim_whitespace=True
+        )
--- find_homologs.py
+++ find_homologs.py
@@ -5,7 +5,13 @@ import functools
 from blasting import BlastnSearch
 import numpy as np
 
+from fractions import Fraction
+
 import pandas as pd
+
+from typing import Callable
+
+from IPython import embed
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -31,20 +37,91 @@ def parse_arguments():
         action="store_true",
         help="keep all pairs in case of a tie"
     )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="hide presumed ortholog distances"
+    )
+    parser.add_argument(
+        "-f",
+        "--report-float",
+        action="store_true",
+        help="report float instead of fraction"
+    )
     return parser.parse_args()
 
-def parse_seq_id(regex, s):
+def parse_seq_id(regex: re.compile, s: pd.Series) -> pd.DataFrame:
+    """Parse a seq_id column to extract the gene and isoform IDs."""
     return s.str.extract(regex).astype(np.int32)
 
-def gene_matches(parse, path1, path2, evalue, n=1):
-    search = BlastnSearch(path1, path2, evalue=evalue)
-    search.hits[["qgene","qiso"]] = parse(search.hits["qseqid"])
-    search.hits[["sgene","siso"]] = parse(search.hits["sseqid"])
+def gene_matches(
+        parse: Callable[[re.compile, pd.Series], pd.DataFrame],
+        path1: str,
+        path2: str,
+        evalue: float,
+        n: int = 1,
+        **blast_kwargs
+) -> pd.DataFrame:
+    """Find the isotigs in file 2 that best match each gene in file 1.
+
+    This function performs a BLAST search of the sequences in the FASTA file
+    located at path1 again the sequences in the FASTA file located at path2.
+
+    For each gene (isotig set) g_1_x in the first file, the top n pairs of 
+    isotigs (i_1_a, i_2_b) such that i_1_a in g_1_x is found, where pairs are
+    compared by their bitscores in the BLAST search. The genes to which the
+    second elements of these pairs belongs may be treated as candidate homologs
+    of g_1_x in sample 2.
+
+    This function must be provided a function to parse the sequence IDs and
+    extract the numerical gene and isotig IDs.
+
+    This function accepts an evalue to be used as a cutoff in the BLAST search.
+    Additional parameters for the BLAST search may also be provided as variadic
+    arguments to this function.
+
+    Parameters:
+        parse:          Function to parse sequence IDs into gene and isotig IDs.
+        path1 (str):    Path to the FASTA file used as the BLAST search query.
+        path2 (str):    Path to the FASTA file used as the BLAST search subject.
+        evalue (float): Expect value cutoff for BLAST search.
+        n (int):        Number of top isotigs to select for each query gene.
+
+    Returns:
+        A dataframe of BLAST hits for subject isotigs best matching query genes.
+    """
+    # TODO: Check whether we really want the top n subject isotigs or the top
+    # n subject genes. (I suspect we really want the latter.)
+    search = BlastnSearch(path1, path2, evalue=evalue, **blast_kwargs)
+    for t in ["q", "s"]:
+        search.hits[[t + "gene", t + "iso"]] = parse(search.hits[t + "seqid"])
     return highest_bitscores(search.hits, n, keep="all")
 
 eprint = functools.partial(print, file=sys.stderr)
 
-def highest_bitscores(df, n=1, groupby="qgene", **kwargs):
+def highest_bitscores(
+        df: pd.DataFrame,
+        n: int = 1,
+        groupby: str = "qgene",
+        **kwargs
+) -> pd.DataFrame:
+    """Select the rows (hits) with the highest bitscore for each group.
+    
+    This function accepts a dataframe containing BLAST hits with bitscores.
+    By default, this function selects the top hit for each query gene, but
+    the column on which to group and the number of hits to select per group may
+    be controlled with the optional parameters.
+
+    Additional keyword arguments will be passed to the pandas nlargest function
+    to control its behavior. This allows the caller to change the strategy for
+    breaking ties, for example.
+
+    Parameters:
+        df:            A dataframe containing BLAST hits.
+        n (int):       Number of top hits to select per group.
+        groupby (str): Column on which to group hits.
+    """
     return df.loc[
         df.groupby(
             groupby
@@ -53,17 +130,23 @@ def highest_bitscores(df, n=1, groupby="qgene", **kwargs):
 
 def main():
     args = parse_arguments()
+    # Partially apply some functions to make the code below less repetitive.
     parse = functools.partial(parse_seq_id, args.regex)
     gm = functools.partial(
         gene_matches,
         parse=parse,
         evalue=args.evalue,
-        n=args.top_n
+        n=args.top_n,
+        additional_columns=["gaps", "nident"]
     )
+    # Search in both the forward and reverse direction to get the isotigs
+    # (and corresponding gene IDs) in one sample that best match genes in the
+    # other.
     eprint("Getting forward matches.")
     forward_matches = gm(path1=args.transcripts1, path2=args.transcripts2)
     eprint("Getting backward matches.")
     backward_matches = gm(path1=args.transcripts2, path2=args.transcripts1)
+    # We rename the columns in the reverse matches to enable merging.
     backward_matches.rename(
         columns={
             a+v : b+v
@@ -72,6 +155,18 @@ def main():
         },
         inplace=True
     )
+    # Compute the "intersection" of the forward and reverse matches.
+    #
+    # A row (hit) in either dataframe with query gene ID q and subject gene ID s
+    # is kept if and only if there is a row with query gene ID q and subject
+    # gene ID s in both dataframes.
+    #
+    # The resulting dataframe has two columns---index_x and index_y---that
+    # indicate where in the original dataframes the rows were found.
+    #
+    # (Keep in mind that we swapped the order order of query and subject in the
+    # reverse dataframe, so "query" is always something in sample 1, and
+    # "subject" is always something in sample 2 from now on.)
     merge_columns = ["qgene", "sgene"]
     intersection = pd.merge(
         forward_matches[merge_columns].reset_index(),
@@ -79,8 +174,13 @@ def main():
         how="inner",
         on=merge_columns
     )
+    # Get the hits (and subject gene ID) with the highest bitscore for each
+    # query gene.
     best_matches = highest_bitscores(
+        # Get the hits with highest bitscore for each pair of homologous genes.
         highest_bitscores(
+            # Get the original rows back from the forward and reverse
+            # dataframes.
             pd.concat(
                 [
                     forward_matches.loc[
@@ -91,14 +191,33 @@ def main():
                     ],
                 ]
             ).reset_index(drop=True),
-            groupby=merge_columns
+            groupby=merge_columns,
+            # TODO: Decide if "all" is the right choice here.
+            keep="all"
         ),
         keep=["first", "all"][args.keep_all]
-    )    
-    dedup = best_matches[merge_columns].drop_duplicates()
-    for match in dedup.itertuples(index=False):
-        print(*match)
+    )
+    # Compute distances at a gene level.
+    best_matches["dist"] = \
+        best_matches["nident"] / (best_matches["length"] - best_matches["gaps"])
+    # Ignore rows with identical query and subject gene IDs.
+    dedup = best_matches[merge_columns + ["dist"]].drop_duplicates()
+    if not args.quiet:
+        for match in dedup.itertuples(index=False):
+            print(*match)
     eprint(f"Found {len(dedup)} matches.")
+    # Compute the overall distance using all identified homologs.
+    #
+    # I use a Fraction here to avoid loss of precision.
+    dist = Fraction(
+        best_matches["nident"].sum(),
+        best_matches["length"].sum() - best_matches["gaps"].sum()
+    )
+    if args.report_float:
+        print(float(dist))
+    else:
+        print(dist)
+    #embed()
     
 if __name__ == "__main__":
     main()
