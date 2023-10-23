a465452768f3715e6ddfa76d35baf63c6b619bc9
actapia <andrew.tapia@uky.edu>
2023-10-23 15:32:07 -0400

    Add new function to phylo_utils

--- phylo_utils.py
+++ phylo_utils.py
@@ -1,4 +1,10 @@
+import Bio.Phylo
 import numpy as np
 
 def tril_jagged(mat):
     return [r[:(i+1)].tolist() for (i, r) in enumerate(np.tril(mat))]
+
+def draw_tree(tree):
+    ax = plt.axes()
+    Bio.Phylo.draw(tree, do_show=False, axes=ax)
+    ax.axis("off")
