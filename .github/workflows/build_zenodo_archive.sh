07c32e134e8205cd70c092117821d98e6f2b8aa3
actapia <andrew.tapia@uky.edu>
2025-02-27 15:46:20 -0500

    Change the way we get Zenodo archive.

--- .github/workflows/build_zenodo_archive.sh
+++ .github/workflows/build_zenodo_archive.sh
@@ -10,3 +10,4 @@ while IFS= read -r -u 5 line; do
 done 5< <(tail -n+2 "$RNA_CLIQUE/docs/tutorials/reads2tree/tall_fescue_accs.csv" | cut -d, -f1)
 python add_remote_files_to_archive.py "$1.zip" --remote "${urls[@]}" --remote-dir "$1/test_data/sra"
 python add_remote_files_to_archive.py "$1.zip" --remote "http://rna-clique-data.s3-website.us-east-2.amazonaws.com/transcripts.zip" --remote-dir "$1/test_data/assemblies" --merge-zips --compress-level 9
+mv "$1.zip" "$RNA_CLIQUE"
