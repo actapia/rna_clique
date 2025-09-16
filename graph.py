9cb8cba4d7a63e0eb30e498d543ff387babc5e77
actapia <andrew.tapia@uky.edu>
2025-09-15 20:05:50 -0400

    Add another misisng file.

---
+++ graph.py
@@ -0,0 +1,20 @@
+import functools
+import networkx as nx
+from typing import Iterator
+
+def connected_component_subgraphs(
+        g,
+        connected_components
+) -> Iterator:
+    """Yields the connected components of the given graph as subgraphs."""
+    for c in connected_components(g):
+        yield g.subgraph(c)
+
+component_subgraphs = functools.partial(
+    connected_component_subgraphs,
+    connected_components=nx.connected_components
+)
+weak_component_subgraphs = functools.partial(
+    connected_component_subgraphs,
+    connected_components=nx.weakly_connected_components
+)
