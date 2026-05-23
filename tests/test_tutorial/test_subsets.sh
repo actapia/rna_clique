46575376e19a162b5aad051f690cea94c6775ae3
actapia <andrew.tapia@uky.edu>
2026-05-23 14:06:07 -0400

    Fix tutorial dir path in test_subsets.

--- tests/test_tutorial/test_subsets.sh
+++ tests/test_tutorial/test_subsets.sh
@@ -12,9 +12,9 @@ conda activate rna-clique
 python make_subset.py -I "$TUTORIAL_DIR"/rna_clique_out/config.yaml \
                       -O "$TUTORIAL_DIR"/infected_subset_out \
                       -y SRR2321388 SRR8003761 SRR7990321 SRR8003736
-! [ -f "tutorial/infected_subset_out/distance_matrix.h5" ]
+! [ -f "$TUTORIAL_DIR/infected_subset_out/distance_matrix.h5" ]
 python filtered_distance.py -O "$TUTORIAL_DIR"/infected_subset_out
-[ -f "tutorial/infected_subset_out/distance_matrix.h5" ]
+[ -f "$TUTORIAL_DIR/infected_subset_out/distance_matrix.h5" ]
 components="$(python plot_component_sizes.py --statistics m \
                      -A "$TUTORIAL_DIR"/infected_subset_out | cut -d' ' -f4)"
 [ "$components" -ge 10000 ]
