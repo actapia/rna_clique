25d0285909739187607c37770487cdce665e9fe8
actapia <andrew.tapia@uky.edu>
2026-06-03 00:22:16 -0400

    cd to home in macos test script.

--- tests/test_install/macos.sh
+++ tests/test_install/macos.sh
@@ -77,6 +77,7 @@ fi
 rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
 set -e
+cd
 git clone -b "$branch" --recurse-submodules https://github.com/actapia/rna_clique
 if [ "$PASSWORDLESS" -ne 1 ] && which sudo && ! sudo -n -v; then
     echo "$password" | sudo -S -v
