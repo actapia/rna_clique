9bcfe0eecb403d18c9769ab759f2166f7c47c40d
actapia <andrew.tapia@uky.edu>
2026-06-06 17:05:11 -0400

    Remove testing directories.

--- parameter_testing/genotype_interleave_test.py
+++
@@ -1,4 +0,0 @@
-from genotype_endophyte_interleave_test import do_group_interleave_test
-
-if __name__ == "__main__":
-    do_group_interleave_test(["genotype"])
--- parameter_testing/stats_for_params.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-{ read -r a; read -r b; } < <(bash do_filtering_step.sh "$@" | tail -n2)
-python plot_component_sizes.py --statistics=m "$a" --samples "$b"
