bbf572e53e48594467defc907ecca455aa67850f
actapia <andrew.tapia@uky.edu>
2026-06-08 19:47:57 -0400

    Add missing -m.

--- tests/test_tutorial/test_subsets.sh
+++ tests/test_tutorial/test_subsets.sh
@@ -14,7 +14,7 @@ python -m rna_clique.make_subset -I "$TUTORIAL_DIR"/rna_clique_out/config.yaml \
 ! [ -f "$TUTORIAL_DIR/infected_subset_out/distance_matrix.h5" ]
 python -m rna_clique.filtered_distance -O "$TUTORIAL_DIR"/infected_subset_out
 [ -f "$TUTORIAL_DIR/infected_subset_out/distance_matrix.h5" ]
-components="$(python rna_clique.plot_component_sizes --statistics m \
-                     -A "$TUTORIAL_DIR"/infected_subset_out | cut -d' ' -f4)"
+components="$(python -m rna_clique.plot_component_sizes --statistics m \
+                        -A "$TUTORIAL_DIR"/infected_subset_out | cut -d' ' -f4)"
 [ "$components" -ge 10000 ]
 
