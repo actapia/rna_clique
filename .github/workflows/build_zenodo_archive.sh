c370d1074e2ed7f0dfc8575f734153928a47d281
actapia <andrew.tapia@uky.edu>
2026-06-11 00:42:17 -0400

    Remove spurious wget in Zenodo build.

--- .github/workflows/build_zenodo_archive.sh
+++ .github/workflows/build_zenodo_archive.sh
@@ -12,5 +12,5 @@ done 5< <(tail -n+2 "$RNA_CLIQUE/docs/tutorials/reads2tree/tall_fescue_accs.csv"
 python add_remote_files_to_archive.py "$1.zip" --remote "${urls[@]}" --remote-dir "$1/test_data/sra"
 python add_remote_files_to_archive.py "$1.zip" --remote "http://rna-clique-data.s3-website.us-east-2.amazonaws.com/transcripts.tar.gz" --remote-dir "$1/test_data/assemblies" --merge-zips --compress-level 9
 python add_remote_files_to_archive.py "$1.zip" --remote "http://rna-clique-data.s3-website.us-east-2.amazonaws.com/trinity_assemblies.tar.xz" --remote-dir "$1/test_data/trinity_assemblies" --merge-zips --compress-level 9
-python add_remote_files_to_archive.py "$1.zip" --remote "wget https://cs.uky.edu/~acta225/rna_clique/ec_genomes.tar.xz" --remote-dir "$1/test_data/ec_genomes" --merge-zips --compress-level 9
+python add_remote_files_to_archive.py "$1.zip" --remote "https://cs.uky.edu/~acta225/rna_clique/ec_genomes.tar.xz" --remote-dir "$1/test_data/ec_genomes" --merge-zips --compress-level 9
 mv "$1.zip" "$RNA_CLIQUE"
